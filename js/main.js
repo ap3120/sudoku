35ee435b03b8a6fd7d507afb413cc86da322092d
dev <ygbe@hotmail.fr>
2024-07-22 23:51:37 +0200

    update algorithm to generate sudoku

--- js/main.js
+++ js/main.js
@@ -105,52 +105,52 @@ window.addEventListener("keydown", function() {
         if (event.which == 49 || event.which == 97){
             element.classList.add('filling_square');
             element.textContent = String(1);
-            sudoList[k][l] = 1;
+            sudokuAsArray[k * 9 + l] = 1;
         }
         if (event.which == 50 || event.which == 98){
             element.classList.add('filling_square');
             element.textContent = String(2);
-            sudoList[k][l] = 2;
+            sudokuAsArray[k * 9 + l] = 2;
         }
         if (event.which == 51 || event.which == 99){
             element.classList.add('filling_square');
             element.textContent = String(3);
-            sudoList[k][l] = 3;
+            sudokuAsArray[k * 9 + l] = 3;
         }
         if (event.which == 52 || event.which == 100){
             element.classList.add('filling_square');
             element.textContent = String(4);
-            sudoList[k][l] = 4;
+            sudokuAsArray[k * 9 + l] = 4;
         }
         if (event.which == 53 || event.which == 101){
             element.classList.add('filling_square');
             element.textContent = String(5);
-            sudoList[k][l] = 5;
+            sudokuAsArray[k * 9 + l] = 5;
         }
         if (event.which == 54 || event.which == 102){
             element.classList.add('filling_square');
             element.textContent = String(6);
-            sudoList[k][l] = 6;
+            sudokuAsArray[k * 9 + l] = 6;
         }
         if (event.which == 55 || event.which == 103){
             element.classList.add('filling_square');
             element.textContent = String(7);
-            sudoList[k][l] = 7;
+            sudokuAsArray[k * 9 + l] = 7;
         }
         if (event.which == 56 || event.which == 104){
             element.classList.add('filling_square');
             element.textContent = String(8);
-            sudoList[k][l] = 8;
+            sudokuAsArray[k * 9 + l] = 8;
         }
         if (event.which == 57 || event.which == 105){
             element.classList.add('filling_square');
             element.textContent = String(9);
-            sudoList[k][l] = 9;
+            sudokuAsArray[k * 9 + l] = 9;
         }
         if (event.which == 46){
             element.classList.add('filling_square');
             element.textContent = "";
-            sudoList[k][l] = 0;
+            sudokuAsArray[k * 9 + l] = 0;
         }
     }
     isSudokuCompleted();
@@ -165,7 +165,7 @@ function handleButton(number){
             let str = selectedSmallSquareId.split(",");
             let k = parseInt(str[0]);
             let l = parseInt(str[1]);
-            sudoList[k][l] = parseInt(number);
+            sudokuAsArray[k * 9 + l] = parseInt(number);
             isSudokuCompleted();
         }
     }
@@ -184,131 +184,115 @@ function generateKeypad(){
     document.getElementById("keyboard").innerHTML = buttonHTML;
 }
 //Create the sudoku
-let list = Array.from({length:9},()=>Array.from({length:9},()=>[1,2,3,4,5,6,7,8,9]));
-let sudoList = Array.from({length:9},()=>[0,0,0,0,0,0,0,0,0]);
-let allLists = [];
-let indexList = [];
-let lengthList = [];
+let sudokuAsArray = [];
 let modifiableListIndex = [];
-let stopLoop = true;
 
-function exitLoop(){
-    stopLoop = false;
-    for (let i=0;i<list.length;i++){
-        for (let j=0;j<list.length;j++){
-            if (list[i][j].length>=0){
-                stopLoop = true;
-            }
-        }
+class LinkedList {
+  constructor(node) {
+    this.head = node;
+    this.tail = node;
+  }
+  getDepth() {
+    let depth = 0;
+    let current = this.head;
+    while(current !== null) {
+      depth += 1;
+      current = current.getNextNode();
     }
-    return stopLoop;
+    return depth;
+  }
+  add(node) {
+    if (this.head === null) this.head = node;
+    this.tail = node;
+  }
 }
 
-function updateList(indexLine,indexColumn){
-    for (let i=0;i<9;i++){
-        for (let j=0;j<9;j++){
-            if (list[i][j].length >= 1){ //filter method can only be applied on array
-                if (3*Math.floor(i/3)+Math.floor(j/3)===3*Math.floor(indexLine/3)+Math.floor(indexColumn/3)){
-                    list[i][j] = list[i][j].filter(elem => elem !== list[indexLine][indexColumn]);
-                }    
-                else if (i===indexLine || j=== indexColumn){
-                    list[i][j] = list[i][j].filter(elem => elem !== list[indexLine][indexColumn]);
-                }
-            }
-        }
-    }
+class Node {
+  constructor (value, parent, possibilities, index) {
+    this.value = value;
+    this.parent = parent;
+    this.possibilities = possibilities;
+    this.index = index;
+    this.child = null;
+  }
+  getNextNode() {
+    return this.child;
+  }
+  setNextNode(child) {
+    this.child = child;
+  }
+  removePossibility(value) {
+    const index = this.possibilities.findIndex(elem => elem === value);
+    if (index > -1) this.possibilities = this.possibilities.splice(index, 1);
+  }
 }
 
-function handleNewIndex(list){
-    let newIndex = [0,0];
-    let maxLength = 9;
-    for (let i=0;i<list.length;i++){
-        for (let j=0;j<list.length;j++){
-            if (list[i][j].length<maxLength && list[i][j].length >0){
-                maxLength = list[i][j].length;
-                newIndex = [i,j];
-            }
-        }
-    }
-    return newIndex;
+const getIndicesFromIndex = (index) => {
+  return {row: Math.floor(index/9), col: index%9};
 }
 
-function isSudokuValid(list){
-    let isValidBool = true;
+const generate = () => {
+  let sudoku = new LinkedList(new Node(Math.floor(Math.random() * 10), null, [1, 2, 3, 4, 5, 6, 7, 8, 9], 0));
+  while(sudoku.getDepth() < 81) {
+    let possibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const tail = sudoku.tail;
+    if (tail !== null) {
+      const newIndex = tail.index + 1;
+      const newIndices = getIndicesFromIndex(newIndex);
+      let currentNode = tail;
+      while (currentNode !== null) {
+        const index = currentNode.index;
+        const indices = getIndicesFromIndex(index);
+        if ((indices.row === newIndices.row || indices.col === newIndices.col || 3 * Math.floor(indices.row/3) + Math.floor(indices.col/3) === 3 * Math.floor(newIndices.row/3) + Math.floor(newIndices.col/3))) {
+          const index = possibilities.findIndex(elem => elem === currentNode.value);
+          if (index > -1) possibilities.splice(index, 1);
+        }
+        currentNode = currentNode.parent;
+      }
+    }
+
+    if (possibilities.length === 0) {
+      let current = tail;
+      while (current.possibilities.length === 1) {
+        if (current === null) {console.log("whaat?");break};
+        current = current.parent;
+      }
+      const index = current.possibilities.findIndex(elem => elem === current.value);
+      current.possibilities.splice(index, 1);
+      current.value = current.possibilities[Math.floor(Math.random() * current.possibilities.length)];
+      current.setNextNode(null);
+      sudoku.tail = current;
+    } else {
+      const node = new Node(possibilities[Math.floor(Math.random() * possibilities.length)], tail, possibilities, tail ? tail.index + 1 : 0);
+      tail.setNextNode(node);
+      sudoku.add(node);
+    }
     
-    list.forEach(row => row.forEach(elem => {
-        if (elem.length === 0){
-            isValidBool = false;
-        }
-    }))
-    return isValidBool;
-}
-
-function handleWrongSudoku(){
-    while (lengthList[lengthList.length-1]<=1){
-        allLists.pop();
-        indexList.pop();
-        lengthList.pop();
-    }
-    let wrongList = allLists.pop();
-    list = allLists[allLists.length-1];
-    indexLine = indexList[indexList.length-1][0];
-    indexColumn = indexList[indexList.length-1][1];
-    let wrongNumber = wrongList[indexLine][indexColumn];
-    for (let i=0;i<list[indexLine][indexColumn].length;i++){
-        if (list[indexLine][indexColumn][i]!=wrongNumber){
-            list[indexLine][indexColumn] = list[indexLine][indexColumn][i];
-        }
-    }
-    updateList(indexLine,indexColumn);
-    if (!isSudokuValid(list)){
-        list = Array.from({length:9},()=>Array.from({length:9},()=>[1,2,3,4,5,6,7,8,9]));
-        allLists = [];
-        indexList = [];
-        lengthList = [];
-    }
-    allLists.push(JSON.parse(JSON.stringify(list)));
-    indexLine = handleNewIndex(list)[0];
-    indexColumn = handleNewIndex(list)[1];
+  }
+  
+  sudokuAsArray = [];
+  let current = sudoku.head;
+  while(current !== null) {
+    sudokuAsArray[current.index] = current.value;
+    current = current.getNextNode();
+  }
 }
 
 function showSudoku(){
-    for (let i=0;i<9;i++){
-        for (let j=0;j<9;j++){
-            let sq = document.getElementById(String(i)+","+String(j));
-            if (Math.random()<0.45){
-                sq.textContent=String(list[i][j]);
-                sudoList[i][j]=list[i][j];
-            }
-            else{
-                modifiableListIndex.push(sq.getAttribute('id'));
-            }
+    for (let i=0;i<sudokuAsArray.length;i++){
+        const indices = getIndicesFromIndex(i);
+        let sq = document.getElementById(String(indices.row)+","+String(indices.col));
+        if (Math.random()<0.45){
+            sq.textContent=String(sudokuAsArray[i]);
+        } else{
+            sudokuAsArray[i] = 0;
+            modifiableListIndex.push(sq.getAttribute('id'));
         }
-    }
-}
-
-function generateSudoku(){
-    let indexLine = Math.floor(Math.random()*9);
-    let indexColumn = Math.floor(Math.random()*9);
-    while (stopLoop == true){
-        indexList.push([indexLine,indexColumn]);
-        lengthList.push(list[indexLine][indexColumn].length);
-        list[indexLine][indexColumn] = list[indexLine][indexColumn][Math.floor(Math.random()*list[indexLine][indexColumn].length)];
-        updateList(indexLine,indexColumn);
-        allLists.push(JSON.parse(JSON.stringify(list)));
-        if (! isSudokuValid(list)){
-            handleWrongSudoku();
-        }
-        else {
-            indexLine = handleNewIndex(list)[0];
-            indexColumn = handleNewIndex(list)[1];
-        }
-        stopLoop = exitLoop();
     }
 }
 
 generateKeypad();
-generateSudoku();
+generate();
 showSudoku();
 
 document.getElementById('erase').addEventListener('click', function(){
@@ -318,7 +302,7 @@ document.getElementById('erase').addEventListener('click', function(){
     let element = document.getElementById(selectedSmallSquareId);
     if (modifiableListIndex.includes(element.getAttribute('id'))){
         element.textContent="";
-        sudoList[i][j] = 0;
+        sudokuAsArray[i * 9 + j] = 0;
     }
 })
 
@@ -362,23 +346,21 @@ document.getElementById("stop_count_up_timer").onclick = function(){stopCounter(
 
 function isSudokuCompleted(){
     let sudokuCompleted = true;
-    for (let i=0;i<9;i++){
-        for (let j=0;j<9;j++){
-            if (sudoList[i][j] == 0){
-                sudokuCompleted = false;
-            }//square value is set to 0 when erase key is pressed
-            for (let k=0;k<9;k++){
-                for (let l=0;l<9;l++){
-                    if (!(i==k && j==l) && (i==k || j==l || 3*Math.floor(i/3)+Math.floor(j/3)===3*Math.floor(k/3)+Math.floor(l/3))){
-                        if (sudoList[i][j] == sudoList[k][l]){
-                            sudokuCompleted = false;
-                        }
-                    }
+    for (let i=0;i<sudokuAsArray.length;i++){
+        const indices = getIndicesFromIndex(i);
+        if (sudokuAsArray[i] === 0){
+            sudokuCompleted = false;
+        }//square value is set to 0 when erase key is pressed
+        for (let k=0;k<sudokuAsArray.length;k++){
+            const indicesToCompare = getIndicesFromIndex(k);
+            if (i !== k && (indices.row === indicesToCompare.row || indices.col === indicesToCompare.col || 3*Math.floor(indices.row/3)+Math.floor(indices.col/3)===3*Math.floor(indicesToCompare.row/3)+Math.floor(indicesToCompare.col/3))){
+                if (sudokuAsArray[i] === sudokuAsArray[k]){
+                    sudokuCompleted = false;
                 }
             }
         }
     }
-    if (sudokuCompleted == true){
+    if (sudokuCompleted){
         stopCounter();
         alert("Congratulation! You've completed the sudoku.");
     }
@@ -396,13 +378,7 @@ document.getElementById('new_game').addEventListener('click',function(){
         stopCounter();
     }
     selectedSmallSquareId = String(4)+','+String(4);
-    list = Array.from({length:9},()=>Array.from({length:9},()=>[1,2,3,4,5,6,7,8,9]));
-    sudoList = Array.from({length:9},()=>[0,0,0,0,0,0,0,0,0]);
-    allLists = [];
-    indexList = [];
-    lengthList = [];
     modifiableListIndex = [];
-    stopLoop = true;
-    generateSudoku();
+    generate();
     showSudoku();
 })
